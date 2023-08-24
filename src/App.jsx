2a8d33f7bc431030eb880cb538b84db31eeee68a
stormymayday <stormymayday7@gmail.com>
2023-08-23 19:17:16 -0700

    Importing Tours in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Loading from "./components/Loading.jsx";
+import Tours from "./components/Tours.jsx";
 
 const url = 'https://course-api.com/react-tours-project';
 
