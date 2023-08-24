e7a255430ebd7f53033b8b9926ef5c33784170bc
stormymayday <stormymayday7@gmail.com>
2023-08-23 19:15:57 -0700

    Fetching data from the url

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useState, useEffect } from "react";
+
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
+
+  const [tours, setTours] = useState(null);
+
+  useEffect(() => {
+
+    const fetchTours = async () => {
+
+      try {
+
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+      } catch (error) {
+
+        console.error(error);
+
+      }
+
+    };
+
+    fetchTours();
+
+  }, []);
+
   return <h2>Tours Starter</h2>;
+
 };
 export default App;
