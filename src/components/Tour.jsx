835a5febfe40a49e3304fd0ebbe34ebeabc80e6c
stormymayday <stormymayday7@gmail.com>
2023-08-23 20:00:49 -0700

    Rendering Tours

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,6 +1,13 @@
-function Tour() {
+function Tour({ id, image, info, name, price }) {
     return (
-        <div>Tour</div>
+        <article className="single-tour">
+            <img src={image} alt={name} className="img" />
+            <span className="tour-price">${price}</span>
+            <div className="tour-info">
+                <h5>{name}</h5>
+                <p>{info}</p>
+            </div>
+        </article>
     );
 }
 
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,6 +1,31 @@
-const Tours = () => {
+import Tour from "./Tour.jsx";
+
+const Tours = ({ tours }) => {
+
+    console.log(tours);
+
     return (
-        <div>Tours</div>
+        <section>
+            <div className="title">
+                <h2>our tours</h2>
+                <div className="title-underline"></div>
+            </div>
+            <div className="tours">
+                {
+                    tours ? (
+
+                        tours.map((tour) => {
+                            return (
+                                <Tour key={tour.id} {...tour} />
+                            );
+                        })
+
+                    ) : (
+                        null
+                    )
+                }
+            </div>
+        </section>
     );
 }
 
