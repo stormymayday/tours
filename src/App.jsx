80f63fa073aa900e49e014c8746a680649928cff
stormymayday <stormymayday7@gmail.com>
2023-12-22 15:39:27 -0800

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,61 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useEffect, useState } from "react";
+
+import Loading from "./components/Loading";
+import Tours from "./components/Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [isError, setIsError] = useState(false);
+
+    const [tours, setTours] = useState([]);
+
+    useEffect(() => {
+        const fetchTours = async () => {
+            // For re-fetching
+            setIsLoading(true);
+
+            try {
+                const response = await fetch(url);
+
+                if (!response.ok) {
+                    setIsError(true);
+                    setIsLoading(false);
+                    return;
+                }
+
+                const data = await response.json();
+
+                setTours(data);
+                // console.log(data);
+            } catch (error) {
+                setIsError(true);
+            }
+
+            setIsLoading(false);
+        };
+
+        fetchTours();
+    }, []);
+
+    if (isLoading) {
+        return (
+            <>
+                <Loading />
+            </>
+        );
+    }
+
+    if (isError) {
+        return (
+            <>
+                <h1>There was an error</h1>
+            </>
+        );
+    }
+
+    return <Tours tours={tours} />;
 };
 export default App;
---
+++ src/components/Tours.jsx
@@ -0,0 +1,7 @@
+import Tour from "./Tour";
+
+const Tours = (props) => {
+    console.log(props.tours);
+    return <div>Tours</div>;
+};
+export default Tours;
