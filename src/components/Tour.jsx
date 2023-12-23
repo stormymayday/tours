50173c8962d53b39ed58d61bc884fc59163ea047
stormymayday <stormymayday7@gmail.com>
2023-12-22 19:19:42 -0800

    Implemented 'read more' and 'show less' functionality

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
+
 const Tour = (props) => {
     const { tour, removeTour } = props;
+
     const { id, image, info, name, price } = tour;
+
+    const [readMore, setReadMore] = useState(false);
+
     return (
         <article className="single-tour">
             <img className="img" src={image} alt={name} />
             <span className="tour-price">${price}</span>
             <div className="tour-info">
                 <h5>{name}</h5>
-                <p>{info}</p>
+                <p>
+                    {readMore ? (
+                        <>
+                            {info}
+                            <button
+                                type="button"
+                                className="info-btn"
+                                style={{ paddingLeft: ".5rem" }}
+                                onClick={() => {
+                                    setReadMore(false);
+                                }}
+                            >
+                                read less
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            {info.substring(0, 255)} ...
+                            <button
+                                type="button"
+                                className="info-btn"
+                                style={{ paddingLeft: ".5rem" }}
+                                onClick={() => {
+                                    setReadMore(true);
+                                }}
+                            >
+                                read more
+                            </button>
+                        </>
+                    )}
+                </p>
             </div>
             <button
                 className="btn btn-block delete-btn"
