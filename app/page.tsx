1d7a9b002197d71fb194a02f3f7d8aff890107c9
stormymayday <stormymayday7@gmail.com>
2024-11-23 13:48:45 -0800

    moved fetchTours into a separate module

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,41 @@
 "use client";
 
+import { TourType } from "@/types";
+import { fetchTours } from "@/utils";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-    const [tours, setTours] = useState([]);
-
-    const fetchTours = async () => {
-        const url = process.env.NEXT_PUBLIC_API_URL;
-        if (url) {
-            const response = await fetch(url);
-            const data = await response.json();
-            setTours(data);
-        }
-    };
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [tours, setTours] = useState<TourType[]>([]);
 
     useEffect(() => {
-        fetchTours();
+        const getData = async () => {
+            setIsLoading(true);
+
+            const { data, isError, errorMessage } = await fetchTours(
+                process.env.NEXT_PUBLIC_API_URL || ""
+            );
+
+            setTours(data);
+            setIsError(isError);
+            setErrorMessage(errorMessage);
+            setIsLoading(false);
+        };
+
+        getData();
     }, []);
 
     console.log(tours);
+
+    if (isLoading) {
+        return <div>Loading</div>;
+    }
+
+    if (isError) {
+        return <div>{errorMessage}</div>;
+    }
 
     return <div>Hello</div>;
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,50 @@
 import { TourType } from "@/types";
 
-export async function fetchTours(url: string): Promise<TourType[]> {
-    // Artificial delay
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-
-    const response = await fetch(url);
-    if (!response.ok) throw new Error("Failed to fetch tours");
-    const data: TourType[] = await response.json();
-    return data;
+interface FetchToursResult {
+    data: TourType[];
+    isError: boolean;
+    errorMessage: string;
 }
+
+export const fetchTours = async (url: string): Promise<FetchToursResult> => {
+    const apiUrl = url;
+
+    if (!apiUrl) {
+        return {
+            data: [],
+            isError: true,
+            errorMessage: "Failed to fetch data",
+        };
+    }
+
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            return {
+                data: [],
+                isError: true,
+                errorMessage: "Failed to fetch data",
+            };
+        }
+
+        const data: TourType[] = await response.json();
+
+        return {
+            data: data,
+            isError: false,
+            errorMessage: "",
+        };
+    } catch (error) {
+        const errorMsg =
+            error instanceof Error
+                ? error.message
+                : "Oops! Something went wrong!";
+
+        return {
+            data: [],
+            isError: true,
+            errorMessage: errorMsg,
+        };
+    }
+};
