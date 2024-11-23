bd33e8d90608b786a9abb9a5a6897960060126fe
stormymayday <stormymayday7@gmail.com>
2024-11-22 19:15:32 -0800

    config optios for the images

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+const url = "https://www.course-api.com/react-tours-project";
+export default async function Home() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
     return (
         <div>
             <h1>Hello!</h1>
